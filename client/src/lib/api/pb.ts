bd041fe0ca1d0d30a7e1a29e7c80963075a88346
Dan Tran <d.k.tran@uq.edu.au>
2025-09-08 16:46:15 +1000

    feat(client): add Docker configuration and update Svelte adapter to Node

--- client/src/lib/api/pb.ts
+++ client/src/lib/api/pb.ts
@@ -3,3 +3,11 @@ import { PUBLIC_PB_URL } from '$env/static/public';
 
 export const pb = new Pocketbase(PUBLIC_PB_URL);
 console.log('[PocketBase] Connected to', PUBLIC_PB_URL);
+pb.health
+	.check()
+	.then(() => {
+		console.log('[PocketBase] Health check passed');
+	})
+	.catch((err) => {
+		console.error('[PocketBase] Health check failed', err);
+	});
